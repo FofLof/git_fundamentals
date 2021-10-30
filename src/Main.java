9f0a83bd85383d17d9d246ebbf188a960bd70bad
Ethan See <ethan_see@hotmail.com>
2021-10-30 12:06:38 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Make a new branch with your name
+        //Fetch
+        //Add a println with your name beneath the existing lines printing names
+        //Commit and push changes to your branch
+        //Merge to Main branch
+    }
+}
