88d20802bdbfa6dbf1cba448b114a86912b274e3
Ethan See <ethan_see@hotmail.com>
2021-10-30 12:16:57 -0700

    Fetch from command line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Make a new branch with your name
-        //Fetch
+        //Fetch with command line
         //Add a println with your name beneath the existing lines printing names
         //Commit and push changes to your branch
         //Merge to Main branch
